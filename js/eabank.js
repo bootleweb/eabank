648cd1f9891b2982fddfe7304f1399ec3991f6ff
Colin Bootle <colin@bootlweb.com>
2015-05-31 13:07:19 +0100

    Able to create new account, with specified currency. Does not show new accounts yet, and no styling. Tweaked the login to use a login object rather than a group of variables polluting the scope.

--- js/eabank.js
+++ js/eabank.js
@@ -2,6 +2,8 @@ var app = angular.module('app', []);
 
 app.controller('AccountsCtrl', function($scope) {
 
+    // $scope.currencies = ["GBP","JPY"];
+
     $scope.currencies = [
         // rates in pennies to prevent rounding of floats
         {
@@ -29,48 +31,71 @@ app.controller('AccountsCtrl', function($scope) {
 
     // obviously keeping usernames and passwords in the clear is daft
     // this is only for proof of concept, and passwords should be hashed and on server side
+    // dongle two factor auth would also be implemented server side, as a separate request
     $scope.users = [{
         firstname: "Colin",
         lastname: "Bootle",
         username: "colin",
         password: "password",
+        dongle: "12345",
         loggedin: false
     }, {
         firstname: "John",
         lastname: "Browne",
         username: "jbrowne",
         password: "jellybabies",
+        dongle: "12345",
         loggedin: false
     }];
 
     $scope.user = '';
-    $scope.username;
-    $scope.password;
-    $scope.login_hint;
+    $scope.login = {};
+    $scope.newaccount = {};
+    $scope.accounts = [];
 
-    $scope.login = function() {
+    $scope.signin = function() {
         var i = 0;
         var max = $scope.users.length;
-        $scope.login_hint = "";
+        $scope.login.hint = "";
         $user = "";
         for (i = 0; i < max; i++) {
-            if ($scope.users[i].username == $scope.username && $scope.users[i].password == $scope.password) {
-                alert("found correct username " + $scope.username);
-
+            if ($scope.users[i].username == $scope.login.username && $scope.users[i].password == $scope.login.password && $scope.users[i].dongle == $scope.login.dongle) {
                 // don't hold onto the username and password in the form!
-                $scope.username = "";
-                $scope.password = "";
+                $scope.login = {};
                 $scope.user = $scope.users[i];
                 break;
             }
         }
         if (!$scope.user) {
-        	$scope.login_hint = "Sorry, login failed";
+            $scope.login.hint = "Sorry, login failed";
         }
     };
 
-    $scope.logout = function() {
-    	$scope.user = '';
-    }
+    $scope.signout = function() {
+        $scope.user = '';
+    };
+
+    $scope.createaccount = function() {
+
+        $scope.newaccount.hint = "";
+
+        if (!$scope.newaccount.accountname) {
+            $scope.newaccount.hint = "Please give your new account a name.";
+            return;
+        };
+        if (!$scope.newaccount.currency) {
+            $scope.newaccount.hint = "Please select a currency.";
+            return;
+        };
+
+        angular.forEach($scope.accounts, function(value, key) {
+            if (value.accountname == $scope.newaccount.accountname) {
+                $scope.newaccount.hint = "Sorry, that account name is already in use.";
+                return;
+            }
+        });
+
+        $scope.accounts.push($scope.newaccount);
+    };
 
 });
